11ec18033170323adab03c367ccb874d67bd4f90
eunbb <fakesilverpark@gmail.com>
2025-02-19 22:57:50 +0900

    250219 : PracticeCKEditor5 controller 에서 Getmapping 으로 주소에따라 뷰단 리턴되도록 구현

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
@@ -1,7 +1,14 @@
 package com.example.practiceckeditor5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ListController {
+
+    @GetMapping("/list")
+    public String listPage() {
+
+        return "list";
+    }
 }
