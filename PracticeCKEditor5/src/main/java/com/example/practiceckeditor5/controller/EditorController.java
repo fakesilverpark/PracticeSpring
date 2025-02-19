10a9df3e1287a1e94cfd1af44a54e806cd5ee342
eunbb <fakesilverpark@gmail.com>
2025-02-20 01:56:24 +0900

    250220 : PracticeCKEditor5 게시글 수정 기능 - 수정할 게시글 불러오기 구현

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/EditorController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/EditorController.java
@@ -1,13 +1,28 @@
 package com.example.practiceckeditor5.controller;
 
+import com.example.practiceckeditor5.service.ContentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class EditorController {
+
+    private final ContentService contentService;
 
     @GetMapping("/editor")
     public String editorPage() {
+
+        return "editor";
+    }
+
+    @GetMapping("/editor/{id}")
+    public String updatePage(@PathVariable int id, Model model){
+
+        model.addAttribute("data", contentService.selectContentById(id));
 
         return "editor";
     }
