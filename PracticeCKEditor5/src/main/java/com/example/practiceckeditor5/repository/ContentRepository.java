87272f9ffdaaadb683f54968993e983c7e801c20
eunbb <fakesilverpark@gmail.com>
2025-02-20 00:01:55 +0900

    250220 : PracticeCKEditor5 ContentRepository create

---
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/repository/ContentRepository.java
@@ -0,0 +1,9 @@
+package com.example.practiceckeditor5.repository;
+
+import com.example.practiceckeditor5.entity.ContentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// @Repository interface 로 하면 스프링 부트가 알아서 관리해서 굳이 붙일 필요가 없다
+public interface ContentRepository extends JpaRepository<ContentEntity, Integer> {
+
+}
