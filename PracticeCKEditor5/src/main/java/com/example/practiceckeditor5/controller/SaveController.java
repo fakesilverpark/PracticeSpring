310a94772ff045a295bcc0019f25c5621236dea3
eunbb <fakesilverpark@gmail.com>
2025-02-19 23:37:32 +0900

    250219 : PracticeCKEditor5 SaveDTO create

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/SaveController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/SaveController.java
@@ -1,7 +1,14 @@
 package com.example.practiceckeditor5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SaveController {
+
+    @PostMapping("/save")
+    public String saveFunc() {
+
+        return "redirect:/";
+    }
 }
---
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/dto/SaveDTO.java
@@ -0,0 +1,11 @@
+package com.example.practiceckeditor5.dto;
+
+import lombok.Data;
+
+@Data // Setter + Getter
+public class SaveDTO {
+
+    private String title;
+
+    private String content;
+}
