bc040f841301994cbf82487633b28cbd633cbe35
eunbb <fakesilverpark@gmail.com>
2025-02-20 00:34:09 +0900

    250220 : PracticeCKEditor5 Service - ListController connect

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
@@ -1,13 +1,20 @@
 package com.example.practiceckeditor5.controller;
 
+import com.example.practiceckeditor5.service.ContentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ListController {
+
+    private ContentService contentService;
 
     @GetMapping("/list")
     public String listPage() {
+
+        contentService.selectContent();
 
         return "list";
     }
