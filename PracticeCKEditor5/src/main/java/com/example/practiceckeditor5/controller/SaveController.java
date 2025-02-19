ca8e0966046378132ad1abfd0223c14f2e389dae
eunbb <fakesilverpark@gmail.com>
2025-02-20 00:25:45 +0900

    250220 : PracticeCKEditor5 Service - Controller connect

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/SaveController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/SaveController.java
@@ -1,16 +1,23 @@
 package com.example.practiceckeditor5.controller;
 
 import com.example.practiceckeditor5.dto.SaveDTO;
+import com.example.practiceckeditor5.service.ContentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class SaveController {
+
+    private final ContentService contentService;
 
     @PostMapping("/save")
     public String saveFunc(SaveDTO saveDTO) {
 
         System.out.println(saveDTO);
+
+        contentService.saveContent(saveDTO);
 
         return "redirect:/";
     }
