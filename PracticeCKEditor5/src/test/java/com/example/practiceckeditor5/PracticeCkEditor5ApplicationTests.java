a488a4de82b4a76bc6726067fc47a52aef1da94b
eunbb <fakesilverpark@gmail.com>
2025-02-19 22:24:58 +0900

    250219 : PracticeCKEditor5 파일 설정

---
+++ PracticeCKEditor5/src/test/java/com/example/practiceckeditor5/PracticeCkEditor5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceckeditor5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeCkEditor5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
