aee0de15e81082128576c882eab4d4573bfb0be1
eunbb <fakesilverpark@gmail.com>
2025-02-20 00:51:21 +0900

    250220 : PracticeCKEditor5 ListController -> list.mustache 로 값 전달해서 출력하기

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/controller/ListController.java
@@ -1,20 +1,24 @@
 package com.example.practiceckeditor5.controller;
 
+import com.example.practiceckeditor5.entity.ContentEntity;
 import com.example.practiceckeditor5.service.ContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class ListController {
 
-    private ContentService contentService;
+    private final ContentService contentService;
 
     @GetMapping("/list")
-    public String listPage() {
+    public String listPage(Model model) {
 
-        contentService.selectContent();
+        model.addAttribute("ContentList", contentService.selectContent());
 
         return "list";
     }
