fec019c4e1185259f3ea099c1ec8490da46ec192
eunbb <fakesilverpark@gmail.com>
2025-02-20 00:19:18 +0900

    250220 : PracticeCKEditor5 Service - Repository connect

--- PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/service/ContentService.java
+++ PracticeCKEditor5/src/main/java/com/example/practiceckeditor5/service/ContentService.java
@@ -1,7 +1,12 @@
 package com.example.practiceckeditor5.service;
 
+import com.example.practiceckeditor5.repository.ContentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ContentService {
+
+    private final ContentRepository contentRepository;
 }
