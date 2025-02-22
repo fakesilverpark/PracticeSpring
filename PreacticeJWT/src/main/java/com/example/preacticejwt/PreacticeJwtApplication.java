86c8ca133c1a7cdb1e1b6b815c9cd4b4c87e2295
eunbb <fakesilverpark@gmail.com>
2025-02-22 23:05:19 +0900

    250222 : PracticeJWT 파일 생성

---
+++ PreacticeJWT/src/main/java/com/example/preacticejwt/PreacticeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.preacticejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreacticeJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreacticeJwtApplication.class, args);
+    }
+
+}
---
+++ PreacticeJWT/src/test/java/com/example/preacticejwt/PreacticeJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preacticejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreacticeJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
