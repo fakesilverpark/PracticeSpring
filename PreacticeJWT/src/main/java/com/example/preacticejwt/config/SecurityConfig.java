c37ebafedb027fdc302e12885db8c18b3d35409c
eunbb <fakesilverpark@gmail.com>
2025-02-22 23:44:02 +0900

    250222 : PracticeJWT 세션 설정

--- PreacticeJWT/src/main/java/com/example/preacticejwt/config/SecurityConfig.java
+++ PreacticeJWT/src/main/java/com/example/preacticejwt/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,6 +27,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("login", "/", "join").permitAll()
                         .anyRequest().authenticated());
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
