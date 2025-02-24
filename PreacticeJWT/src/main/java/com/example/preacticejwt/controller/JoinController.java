d938ab2f63728b97e6e4a04153658a46256661be
eunbb <fakesilverpark@gmail.com>
2025-02-24 19:58:57 +0900

    250224 : PracticeJWT JoinDTO 작성

---
+++ PreacticeJWT/src/main/java/com/example/preacticejwt/controller/JoinController.java
@@ -0,0 +1,4 @@
+package com.example.preacticejwt.controller;
+
+public class JoinController {
+}
---
+++ PreacticeJWT/src/main/java/com/example/preacticejwt/dto/JoinDTO.java
@@ -0,0 +1,11 @@
+package com.example.preacticejwt.dto;
+
+import lombok.Data;
+
+@Data
+public class JoinDTO {
+
+    private String username;
+
+    private String password;
+}
