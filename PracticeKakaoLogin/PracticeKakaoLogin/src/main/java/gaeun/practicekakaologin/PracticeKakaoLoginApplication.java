216ef2735c06dfa6a6f07bb0face71ee602c1c2c
eunbb <fakesilverpark@gmail.com>
2025-06-04 15:49:03 +0900

    first

---
+++ PracticeKakaoLogin/PracticeKakaoLogin/src/main/java/gaeun/practicekakaologin/PracticeKakaoLoginApplication.java
@@ -0,0 +1,13 @@
+package gaeun.practicekakaologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeKakaoLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeKakaoLoginApplication.class, args);
+    }
+
+}
---
+++ PracticeKakaoLogin/PracticeKakaoLogin/src/test/java/gaeun/practicekakaologin/PracticeKakaoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package gaeun.practicekakaologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeKakaoLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
